84fb1c7cc8f36ea0bde0b13f362a68df3c93b231
prabhu <prabhudeveloper03.com>
2024-03-21 22:07:13 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./components/Dashboard";
+
+
+const MyApp = () => { 
+  return (
+    <div className="container w-full mx-auto ">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default MyApp; 
